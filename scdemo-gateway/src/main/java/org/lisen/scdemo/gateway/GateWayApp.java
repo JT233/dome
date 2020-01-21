2efbdfa38c5f229b9c5f151bd0eb24e755d97823
lisen <lisensir@qq.com>
2020-01-21 19:43:02 +0800

    增加zuul网关配置，监控，jwt令牌支持，ribbonconsumer服务的疾控支持

--- scdemo-gateway/src/main/java/org/lisen/scdemo/gateway/GateWayApp.java
+++ scdemo-gateway/src/main/java/org/lisen/scdemo/gateway/GateWayApp.java
@@ -2,7 +2,9 @@ package org.lisen.scdemo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -12,6 +14,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableHystrix
+@EnableCircuitBreaker
 public class GateWayApp {
 
     public static void main(String[] args) {
